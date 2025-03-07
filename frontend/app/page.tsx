0855934822ff1443862300453bc87154739b7446
ting436 <tinghui1804@gmail.com>
2025-03-07 12:20:11 +0800

    removed cleanup call

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,14 +9,7 @@ export default function Home() {
     // Clear all storage
     localStorage.clear()
     
-    // Call cleanup API
-    fetch('http://127.0.0.1:8000/api/cleanup', {
-      method: 'POST',
-    })
-    .then(() => {
-      router.push('/dashboard/')
-    })
-    .catch(console.error)
+    router.push('/dashboard/')
   }, [])
 
   return <div>Loading...</div>
